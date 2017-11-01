18a15c64df05e495bae2da349af12c7b7025b50d
MTRIGUEROS <moisestrigueros@hotmail.com>
2017-10-31 22:29:25 -0600

    modificacion a app module

--- sistemaWeb/src/app/app.module.ts
+++ sistemaWeb/src/app/app.module.ts
@@ -4,18 +4,11 @@ import { Routes,RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 import { NotFound404Component } from './components/not-found-404/not-found-404.component';
 import { MenuComponent } from './components/menu/menu.component';
 import {MenuModule} from "./components/menu/menu.module";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
-import { DataTablesModule } from 'angular-datatables';
-import { AddProductoComponent } from './components/producto/add-producto/add-producto.component';
-import { AddUsuarioComponent } from './components/usuario/add-usuario/add-usuario.component';
-import { TrabajadorComponent } from './components/trabajador/trabajador.component';
-import { BodegaSucursalComponent } from './components/bodega-sucursal/bodega-sucursal.component';
-
 
 
 
--- sistemaWeb/src/app/models/Trabajador.ts
+++ sistemaWeb/src/app/models/Trabajador.ts
@@ -1,12 +1,11 @@
 export class Trabajador{
 	constructor(
-
 		public IdTrabajador : number
 		,public IdSucursal: number
 		,public IdCargo: number
 		,public Nombres: string
 		,public Apellidos: string
-		,public NumeroCedula: string 
+		,public NumeroCedula: string
 		,public FechaNacimiento: string
 		,public Direccion: string
 		,public FechaIngreso: number

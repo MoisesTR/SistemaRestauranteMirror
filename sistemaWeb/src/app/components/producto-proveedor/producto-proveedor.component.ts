52b55faf514647b73b80e118c15621d6c2fa65e9
Jake Zeledón <jackiezeledon@gmail.com>
2017-11-12 05:52:47 -0600

    Agregar nueva pantalla producto-proveedor

--- sistemaWeb/src/app/components/producto-proveedor/producto-proveedor.component.ts
+++ sistemaWeb/src/app/components/producto-proveedor/producto-proveedor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ProductoProveedorService} from '../../services/producto-proveedor.service';
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProductoProveedor} from "../../models/ProductoProveedor";
+declare var $:any;
 
 @Component({
   selector: 'app-producto-proveedor',
@@ -24,7 +25,40 @@ export class ProductoProveedorComponent implements OnInit {
 
   ngOnInit() {
 
+    var IdProducto = [
+      "Producto 1"      
+      , "Producto 2"
+    ];
 
+    var IProveedor = [
+      "Cainsa"
+      , "Tiptop"
+
+    ];
+
+    $(document).ready(function(){
+         
+      $('.autocomplete-producto').mdb_autocomplete({
+          data: IdProducto
+      });  
+      
+      $('.autocomplete-proveedor').mdb_autocomplete({
+        data: IProveedor
+      });    
+
+      $(".selectempaque").select2({
+        maximumSelectionLength: 1
+      });
+
+      $(".selectunidadmedida").select2({
+        maximumSelectionLength: 1
+      });
+
+      $(".selectenvase").select2({
+        maximumSelectionLength: 1
+      });
+
+    });
   }
 
   

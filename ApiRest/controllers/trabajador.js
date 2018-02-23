2f1435139e7e6f9e77497eb74e8c78e00c4fa969
mtrigueros <moisestrigueros@hotmail.com>
2018-02-23 01:33:40 -0600

    Incorporar metodos para obtener y guardar imagen a un trabajador en la api

--- ApiRest/controllers/trabajador.js
+++ ApiRest/controllers/trabajador.js
@@ -1,96 +1,153 @@
-const {mssqlErrors} = require('../Utils/util')
+const { mssqlErrors } = require('../Utils/util')
 const { matchedData, sanitize } = require('express-validator/filter');
+var fs = require('fs');
+var path = require('path');
 const db = require('../services/database');
 const sql = require('mssql')
 
-function getTrabajadorById(req,res){
+function getTrabajadorById(req, res) {
     var data = req.params;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdTrabajador',sql.Int,data.IdSucursal);
+    db.pushAOJParam(aoj, 'IdTrabajador', sql.Int, data.IdSucursal);
     db.storedProcExecute('USP_GET_TRABAJADOR', aoj)
-    .then((results) => {
-        res.status(200).json({ trabajador:results.recordset[0] }) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ trabajador: results.recordset[0] })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function getTrabajadores(req,res){
+
+function getTrabajadores(req, res) {
     let Habilitado = req.query.Habilitado
     var aoj = [];
-    db.pushAOJParam(aoj, 'Habilitado',sql.Int,Habilitado);
+    db.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado);
     db.storedProcExecute('USP_GET_TRABAJADORES', aoj)
-    .then((results) => {
-       res.status(200).json({trabajadores:results.recordset}) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ trabajadores: results.recordset })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function createTrabajador(req,res){
-    var trabajadorData=matchedData(req,{locations:['body']});
+
+function createTrabajador(req, res) {
+    var trabajadorData = matchedData(req, { locations: ['body'] });
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdSucursal',sql.Int,trabajadorData.IdSucursal);
-    db.pushAOJParam(aoj, 'IdCargo',sql.Int,trabajadorData.IdCargo);
-    db.pushAOJParam(aoj, 'Nombres',sql.NVarChar(50),trabajadorData.Nombres);
-    db.pushAOJParam(aoj, 'Apellidos',sql.NVarChar(50),trabajadorData.Apellidos);
-    db.pushAOJParam(aoj, 'NumeroCedula',sql.NVarChar(50),trabajadorData.NumeroCedula);
+    db.pushAOJParam(aoj, 'IdSucursal', sql.Int, trabajadorData.IdSucursal);
+    db.pushAOJParam(aoj, 'IdCargo', sql.Int, trabajadorData.IdCargo);
+    db.pushAOJParam(aoj, 'Nombres', sql.NVarChar(50), trabajadorData.Nombres);
+    db.pushAOJParam(aoj, 'Apellidos', sql.NVarChar(50), trabajadorData.Apellidos);
+    db.pushAOJParam(aoj, 'NumeroCedula', sql.NVarChar(50), trabajadorData.NumeroCedula);
     db.pushAOJParam(aoj, 'Imagen', sql.NVarChar(50), trabajadorData.Imagen);
-    db.pushAOJParam(aoj, 'FechaNacimiento',sql.Date,trabajadorData.FechaNacimiento);
-    db.pushAOJParam(aoj, 'Direccion',sql.NVarChar(300),trabajadorData.Direccion);
-    db.pushAOJParam(aoj, 'Telefono1', sql.NVarChar(20), trabajadorData.Telefono1);
-    db.pushAOJParam(aoj, 'Telefono2', sql.NVarChar(20), trabajadorData.Telefono2);
-    db.pushAOJParam(aoj, 'FechaIngreso',sql.Date,trabajadorData.FechaIngreso);
-    db.pushOutParam(aoj, 'IdTrabajador', sql.Int)
-    db.storedProcExecute('USP_CREATE_TRABAJADOR',aoj)
-    .then((results) => {
-        res.status(200).json(results.output)
-    }).catch((err) => {
-       res.status(500).json( mssqlErrors(err) ); 
-    });
-}
-function updateTrabajador(req,res){
-    var trabajadorData = matchedData(req);
-    var aoj = [];
-    db.pushAOJParam(aoj, 'IdSucursal',sql.Int,trabajadorData.IdSucursal);
-    db.pushAOJParam(aoj, 'IdCargo',sql.Int,trabajadorData.IdCargo);
-    db.pushAOJParam(aoj, 'Nombres',sql.NVarChar(50),trabajadorData.Nombres);
-    db.pushAOJParam(aoj, 'Apellidos',sql.NVarChar(50),trabajadorData.Apellidos);
-    db.pushAOJParam(aoj, 'NumeroCedula',sql.NVarChar(50),trabajadorData.NumeroCedula);
-    db.pushAOJParam(aoj, 'Imagen', sql.NVarChar(50), trabajadorData.Imagen);
-    db.pushAOJParam(aoj, 'FechaNacimiento',sql.Date,trabajadorData.FechaNacimiento);
+    db.pushAOJParam(aoj, 'FechaNacimiento', sql.Date, trabajadorData.FechaNacimiento);
     db.pushAOJParam(aoj, 'Direccion', sql.NVarChar(300), trabajadorData.Direccion);
     db.pushAOJParam(aoj, 'Telefono1', sql.NVarChar(20), trabajadorData.Telefono1);
     db.pushAOJParam(aoj, 'Telefono2', sql.NVarChar(20), trabajadorData.Telefono2);
-    db.pushAOJParam(aoj, 'FechaIngreso',sql.Date, trabajadorData.FechaIngreso);
+    db.pushAOJParam(aoj, 'FechaIngreso', sql.Date, trabajadorData.FechaIngreso);
+    db.pushOutParam(aoj, 'IdTrabajador', sql.Int)
+    db.storedProcExecute('USP_CREATE_TRABAJADOR', aoj)
+        .then((results) => {
+            res.status(200).json(results.output)
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
+}
+
+function updateTrabajador(req, res) {
+    var trabajadorData = matchedData(req);
+    var aoj = [];
+    db.pushAOJParam(aoj, 'IdSucursal', sql.Int, trabajadorData.IdSucursal);
+    db.pushAOJParam(aoj, 'IdCargo', sql.Int, trabajadorData.IdCargo);
+    db.pushAOJParam(aoj, 'Nombres', sql.NVarChar(50), trabajadorData.Nombres);
+    db.pushAOJParam(aoj, 'Apellidos', sql.NVarChar(50), trabajadorData.Apellidos);
+    db.pushAOJParam(aoj, 'NumeroCedula', sql.NVarChar(50), trabajadorData.NumeroCedula);
+    db.pushAOJParam(aoj, 'Imagen', sql.NVarChar(50), trabajadorData.Imagen);
+    db.pushAOJParam(aoj, 'FechaNacimiento', sql.Date, trabajadorData.FechaNacimiento);
+    db.pushAOJParam(aoj, 'Direccion', sql.NVarChar(300), trabajadorData.Direccion);
+    db.pushAOJParam(aoj, 'Telefono1', sql.NVarChar(20), trabajadorData.Telefono1);
+    db.pushAOJParam(aoj, 'Telefono2', sql.NVarChar(20), trabajadorData.Telefono2);
+    db.pushAOJParam(aoj, 'FechaIngreso', sql.Date, trabajadorData.FechaIngreso);
     db.pushAOJParam(aoj, 'IdTrabajador', sql.Int, trabajadorData.IdTrabajador);
-    db.storedProcExecute('USP_UPDATE_TRABAJADOR',aoj)
-    .then((results) => {
-        res.status(200).json({
-            success:'Trabajador actualizado con exito!'
-        })
-    }).catch((err) => {
-       res.status(500).json( mssqlErrors(err) ); 
-    });
+    db.storedProcExecute('USP_UPDATE_TRABAJADOR', aoj)
+        .then((results) => {
+            res.status(200).json({
+                success: 'Trabajador actualizado con exito!'
+            })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function changeStateTrabajador(req,res){
+
+function changeStateTrabajador(req, res) {
     var data = matchedData(req);
-    var aoj = [];   
+    var aoj = [];
     console.log('Changing state')
-    db.pushAOJParam(aoj, 'IdProducto',sql.Int,data.IdTrabajador)
-    db.pushAOJParam(aoj, 'Habilitado',sql.Int,Habilitado)
+    db.pushAOJParam(aoj, 'IdProducto', sql.Int, data.IdTrabajador)
+    db.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado)
     db.storedProcExecute('USP_DISP_TRABAJADOR', aoj)
-    .then((results) => {
-        res.status(200).json({
-            success:'Trabajador '+(Habilitado) ? 'Habilitado' : 'Deshabilitado' +' con exito!'
-        })
-        console.log('Trabajador '+(Habilitado) ? 'Habilitado' : 'Deshabilitado' +' con exito!')
-    }).catch((err) => {
-       res.status(500).json( mssqlErrors(err) ); 
-    });
+        .then((results) => {
+            res.status(200).json({
+                success: 'Trabajador ' + (Habilitado) ? 'Habilitado' : 'Deshabilitado' + ' con exito!'
+            })
+            console.log('Trabajador ' + (Habilitado) ? 'Habilitado' : 'Deshabilitado' + ' con exito!')
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-module.exports={
-   createTrabajador,
-   getTrabajadores,
-   getTrabajadorById,
-   updateTrabajador,
-   changeStateTrabajador
+
+
+function uploadImage(req, res) {
+    var IdProducto = req.params.IdTrabajador;
+    var file_name = 'No Subido...';
+
+    if (req.files) {
+
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+            res.status(200).send({ image: file_name });
+        } else {
+            fs.unlink(file_path, (err) => {
+                if (err) {
+                    res.status(200).send({ message: 'Extension no valida, el fichero no se pudo borrar' });
+                } else {
+                    res.status(200).send({ message: 'Extension no valida' });
+                }
+            });
+            res.status(200).send({ message: 'Extension no valida' });
+        }
+
+    } else {
+        res.status(200).send({ message: 'No se han subido archivos' });
+    }
+
+
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/trabajadores/' + imageFile;
+
+    fs.exists(path_file, function(exists) {
+        if (exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(404).send({ message: 'La imagen no existe' });
+        }
+    });
+
+}
+
+module.exports = {
+    createTrabajador,
+    getTrabajadores,
+    getTrabajadorById,
+    updateTrabajador,
+    changeStateTrabajador,
+    uploadImage,
+    getImageFile
 }
--- ApiRest/routes/api.js
+++ ApiRest/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express')
-const CategoriaController= require('../controllers/categoria')
+const CategoriaController = require('../controllers/categoria')
 const ClasificacionController = require('../controllers/clasificacion')
-const EmpaqueController =require('../controllers/empaque')
+const EmpaqueController = require('../controllers/empaque')
 const EnvaseController = require('../controllers/envase')
 const ProveedorController = require('../controllers/proveedor')
 const SubclasificacionController = require('../controllers/subclasificacion')
@@ -22,119 +22,121 @@ const AuthController = require('../controllers/auth')
 
 var Router = express.Router()
 var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './uploads/productos'});
+var md_upload_producto = multipart({ uploadDir: './uploads/productos' });
+var md_upload_trabajador = multipart({ uploadDir: './uploads/trabajadores' });
 
 Router
-    .get('/',(req,res) => {
-       res.status(200).json({
-           isApi:true
-       }) 
+    .get('/', (req, res) => {
+        res.status(200).json({
+            isApi: true
+        })
     })
     //Rutas categoria controller
-    .get('/categoria/:IdCategoria(\\d+)',CategoriaController.getCategoriaById)
-    .get('/categorias',CategoriaController.getCategorias)
-    .post('/categoria',CategoriaController.createCategoria)
-    .put('/categoria',CategoriaController.updateCategoria)
-    .delete('/categoria/:IdCategoria(\\d+)',CategoriaController.changeStateCategoria)
+    .get('/categoria/:IdCategoria(\\d+)', CategoriaController.getCategoriaById)
+    .get('/categorias', CategoriaController.getCategorias)
+    .post('/categoria', CategoriaController.createCategoria)
+    .put('/categoria', CategoriaController.updateCategoria)
+    .delete('/categoria/:IdCategoria(\\d+)', CategoriaController.changeStateCategoria)
     //Rutas clasificacion controller
-    .get('/clasificacion/:IdClasificacion(\\d+)',ClasificacionController.getClasificacionById)
-    .get('/clasificaciones',ClasificacionController.getClasificaciones)
-    .post('/clasificacion',ClasificacionController.createClasificacion)
-    .put('/clasificacion',ClasificacionController.updateClasificacion)
-    .delete('/clasificacion/:IdClasificacion(\\d+)',ClasificacionController.changeStateClasificacion)
+    .get('/clasificacion/:IdClasificacion(\\d+)', ClasificacionController.getClasificacionById)
+    .get('/clasificaciones', ClasificacionController.getClasificaciones)
+    .post('/clasificacion', ClasificacionController.createClasificacion)
+    .put('/clasificacion', ClasificacionController.updateClasificacion)
+    .delete('/clasificacion/:IdClasificacion(\\d+)', ClasificacionController.changeStateClasificacion)
     //Rutas empaque controller
-    .get('/empaque/:IdEmpaque(\\d+)',EmpaqueController.getEmpaqueById)
-    .get('/empaques',EmpaqueController.getEmpaques)
-    .post('/empaque',EmpaqueController.createEmpaque)
+    .get('/empaque/:IdEmpaque(\\d+)', EmpaqueController.getEmpaqueById)
+    .get('/empaques', EmpaqueController.getEmpaques)
+    .post('/empaque', EmpaqueController.createEmpaque)
     //Rutas envase controler
-    .get('/envase/:IdEnvase(\\d+)',EnvaseController.getEnvaseById)
-    .get('/envases',EnvaseController.getEnvases)
-    .post('/envase',EnvaseController.createEnvase)
+    .get('/envase/:IdEnvase(\\d+)', EnvaseController.getEnvaseById)
+    .get('/envases', EnvaseController.getEnvases)
+    .post('/envase', EnvaseController.createEnvase)
     //Rutas proveedor Controller
-    .get('/proveedor/:IdProveedor(\\d+)',ProveedorController.getProveedorById)
-    .get('/proveedores',ProveedorController.getProveedores)
-    .post('/proveedor',ProveedorController.createProveedor)
-    .put('/proveedor',ProveedorController.updateProveedor)
-    .delete('/proveedor/:IdProveedor(\\d+)',ProveedorController.changeStateProveedor)
+    .get('/proveedor/:IdProveedor(\\d+)', ProveedorController.getProveedorById)
+    .get('/proveedores', ProveedorController.getProveedores)
+    .post('/proveedor', ProveedorController.createProveedor)
+    .put('/proveedor', ProveedorController.updateProveedor)
+    .delete('/proveedor/:IdProveedor(\\d+)', ProveedorController.changeStateProveedor)
 
-    //Rutas subclasificacion Controller
-    .get('/subclasificacion/:IdSubclasificacion(\\d+)',SubclasificacionController.getSubclasificacionById)
-    .get('/subclasificaciones',SubclasificacionController.getSubclasificaciones)
-    .get('/subclasificaciones/:IdClasificacion(\\d+)',SubclasificacionController.getSubclasificacionesByIdClasificacion)
-    .post('/subclasificacion',SubclasificacionController.createSubclasificacion)
-	.put('/subclasificacion',SubclasificacionController.updateSubclasificacion)
-    .delete('/subclasificacion/:IdSubClasificacion(\\d+)',SubclasificacionController.changeStateSubClasificacion)
+//Rutas subclasificacion Controller
+.get('/subclasificacion/:IdSubclasificacion(\\d+)', SubclasificacionController.getSubclasificacionById)
+    .get('/subclasificaciones', SubclasificacionController.getSubclasificaciones)
+    .get('/subclasificaciones/:IdClasificacion(\\d+)', SubclasificacionController.getSubclasificacionesByIdClasificacion)
+    .post('/subclasificacion', SubclasificacionController.createSubclasificacion)
+    .put('/subclasificacion', SubclasificacionController.updateSubclasificacion)
+    .delete('/subclasificacion/:IdSubClasificacion(\\d+)', SubclasificacionController.changeStateSubClasificacion)
     //Rutas estadoproducto controller
-    .get('/estadosproductos',EstadoProductoController.getEstados)
-    .get('/estadoproducto/:IdEstado(\\d+)',EstadoProductoController.getEstadoById)
+    .get('/estadosproductos', EstadoProductoController.getEstados)
+    .get('/estadoproducto/:IdEstado(\\d+)', EstadoProductoController.getEstadoById)
     //Rutas producto controller
-    .get('/productos',ProductoController.getProductos)
-    .get('/producto/:IdProducto(\\d+)',ProductoController.getProductoById)
-    .post('/productoUploadImage',[md_upload],ProductoController.uploadImage)
-    .get('/productoGetImage/:imageFile',ProductoController.getImageFile)
-    .post('/producto',ProductoController.createProducto)
-    .put('/producto',ProductoController.updateProducto)
-    .delete('/producto/:IdProducto(\\d+)',ProductoController.changeStateProducto)
+    .get('/productos', ProductoController.getProductos)
+    .get('/producto/:IdProducto(\\d+)', ProductoController.getProductoById)
+    .post('/productoUploadImage', [md_upload_producto], ProductoController.uploadImage)
+    .get('/productoGetImage/:imageFile', ProductoController.getImageFile)
+    .post('/producto', ProductoController.createProducto)
+    .put('/producto', ProductoController.updateProducto)
+    .delete('/producto/:IdProducto(\\d+)', ProductoController.changeStateProducto)
 
-    //Rutas sucursal Controller
-    .get('/sucursales',SucursalController.getSucursales)
-    .get('/sucursal/:IdSucursal(\\d+)',SucursalController.getSucursalById)
-    .post('/sucursal',SucursalController.createSucursal)
-    .post('/sucursal/:IdSucursal(\\d+)/telefono',SucursalController.createTelefonoSucursal)
-    .get('/sucursal/:IdSucursal(\\d+)/telefono/:IdTelefonoSucursal(\\d+)',SucursalController.getTelefonoSucursal)
-    .put('/sucursal/:IdSucursal(\\d+)/telefono/:IdTelefonoSucursal(\\d+)',SucursalController.updateTelefonoSucursal)
-    .get('/sucursal/:IdSucursal(\\d+)/telefonos',SucursalController.getTelefonosBySucursalId)
+//Rutas sucursal Controller
+.get('/sucursales', SucursalController.getSucursales)
+    .get('/sucursal/:IdSucursal(\\d+)', SucursalController.getSucursalById)
+    .post('/sucursal', SucursalController.createSucursal)
+    .post('/sucursal/:IdSucursal(\\d+)/telefono', SucursalController.createTelefonoSucursal)
+    .get('/sucursal/:IdSucursal(\\d+)/telefono/:IdTelefonoSucursal(\\d+)', SucursalController.getTelefonoSucursal)
+    .put('/sucursal/:IdSucursal(\\d+)/telefono/:IdTelefonoSucursal(\\d+)', SucursalController.updateTelefonoSucursal)
+    .get('/sucursal/:IdSucursal(\\d+)/telefonos', SucursalController.getTelefonosBySucursalId)
     .put('/sucursal/:IdSucursal(\\d+)')
     //Rutas para unidad de Medida Controller
-    .get('/unidadesmedida',UnidadMedidaController.getUnidadesMedida)
-    .get('/unidadmedida/:IdUnidadMedida(\\d+)',UnidadMedidaController.getUnidadById)
-    .post('/unidadmedida',UnidadMedidaController.createUnidadMedida)
-    .put('/unidadmedida',UnidadMedidaController.updateUDM)
-    .delete('/unidadmedida/:IdUnidadMedida(\\d+)',UnidadMedidaController.changeStateUnidadMedida)
+    .get('/unidadesmedida', UnidadMedidaController.getUnidadesMedida)
+    .get('/unidadmedida/:IdUnidadMedida(\\d+)', UnidadMedidaController.getUnidadById)
+    .post('/unidadmedida', UnidadMedidaController.createUnidadMedida)
+    .put('/unidadmedida', UnidadMedidaController.updateUDM)
+    .delete('/unidadmedida/:IdUnidadMedida(\\d+)', UnidadMedidaController.changeStateUnidadMedida)
     //Rutas para Producto Proveedor
-    .get('/productos/proveedores',ProductoProveedorController.getProductosProveedores)
-    .get('/producto/proveedor/:IdProductoProveedor(\\d+)',ProductoProveedorController.getProductoProveedorById)
-    .get('/productos/proveedor/:IdProveedor(\\d+)',ProductoProveedorController.getProductoProveedorById)
-    .get('/producto/proveedores/:IdProducto(\\d+)',ProductoProveedorController.getProveedoresOfProducto)
-    .post('/producto/proveedor',validations.createProductoProveedor,validations.validsParams,ProductoProveedorController.createProductoProveedor)
-    .put('/producto/proveedor',ProductoProveedorController.changeStateProductoProveedor)
+    .get('/productos/proveedores', ProductoProveedorController.getProductosProveedores)
+    .get('/producto/proveedor/:IdProductoProveedor(\\d+)', ProductoProveedorController.getProductoProveedorById)
+    .get('/productos/proveedor/:IdProveedor(\\d+)', ProductoProveedorController.getProductoProveedorById)
+    .get('/producto/proveedores/:IdProducto(\\d+)', ProductoProveedorController.getProveedoresOfProducto)
+    .post('/producto/proveedor', validations.createProductoProveedor, validations.validsParams, ProductoProveedorController.createProductoProveedor)
+    .put('/producto/proveedor', ProductoProveedorController.changeStateProductoProveedor)
     //.delete('/producto/proveedor')
     //Rutas para Rol Controller
-    .post('/rol',RoleController.createRol)
-    .get('/roles',RoleController.getRoles)
-    .get('/rol/:IdRol(\\d+)',RoleController.getRolbyId)
-    .put('/rol',RoleController.updateRol)
+    .post('/rol', RoleController.createRol)
+    .get('/roles', RoleController.getRoles)
+    .get('/rol/:IdRol(\\d+)', RoleController.getRolbyId)
+    .put('/rol', RoleController.updateRol)
     //Rutas clasificacionUnidadmedida controller
-    .get('/clasificacionunidadmedida/:IdClasificacionUnidadMedida(\\d+)',clasificacionUnidadMedidaController.getClasificacionUdmById)
-    .get('/clasificacionesunidadmedida',clasificacionUnidadMedidaController.getClasificacionesUdm)
+    .get('/clasificacionunidadmedida/:IdClasificacionUnidadMedida(\\d+)', clasificacionUnidadMedidaController.getClasificacionUdmById)
+    .get('/clasificacionesunidadmedida', clasificacionUnidadMedidaController.getClasificacionesUdm)
     /*********** faltan */
     //Rutas para Trabajador Controller
-    .get('/trabajadores',TrabajadorController.getTrabajadores)
-    .post('/trabajador',validations.createTrabajador,validations.validsParams,TrabajadorController.createTrabajador)
-    .get('/trabajador/:IdTrabajador(\\d+)',TrabajadorController.getTrabajadorById)
-    .put('/trabajador',validations.createTrabajador.concat(validations.updateTrabajador),validations.validsParams,TrabajadorController.updateTrabajador)
-    .delete('/trabajador/:IdTrabajador(\\d+)',validations.deleteTrabajador,validations.validsParams,TrabajadorController.changeStateTrabajador)
+    .get('/trabajadores', TrabajadorController.getTrabajadores)
+    .post('/trabajador', validations.createTrabajador, validations.validsParams, TrabajadorController.createTrabajador)
+    .get('/trabajador/:IdTrabajador(\\d+)', TrabajadorController.getTrabajadorById)
+    .put('/trabajador', validations.createTrabajador.concat(validations.updateTrabajador), validations.validsParams, TrabajadorController.updateTrabajador)
+    .delete('/trabajador/:IdTrabajador(\\d+)', validations.deleteTrabajador, validations.validsParams, TrabajadorController.changeStateTrabajador)
+    .post('/trabajadorUploadImage', [md_upload_trabajador], TrabajadorController.uploadImage)
+    .get('/trabajadorGetImage/:imageFile', TrabajadorController.getImageFile)
 
-    //Rutas Para Cargo Controller
-    .get('/cargos',CargoController.getCargos)
-    .post('/cargo',validations.createCargo, validations.validsParams , CargoController.createCargo)
-    .get('/cargo/:IdCargo(\\d+)',CargoController.getCargoById)
-    .put('/cargo',CargoController.updateCargo)
-    .delete('/cargo/:IdCargo(\\d+)',CargoController.changeStateCargo)
+//Rutas Para Cargo Controller
+.get('/cargos', CargoController.getCargos)
+    .post('/cargo', validations.createCargo, validations.validsParams, CargoController.createCargo)
+    .get('/cargo/:IdCargo(\\d+)', CargoController.getCargoById)
+    .put('/cargo', CargoController.updateCargo)
+    .delete('/cargo/:IdCargo(\\d+)', CargoController.changeStateCargo)
 
-    //Rutas para Bodega Area Produccion
-    .post('/entradabodegaap',validations.createEntradaBodegaAP,validations.validsParams,bodegaApController.createEntradaBodegaAp)
-    .post('/detalleentradabodegaap',validations.detalleEntradaBodega,validations.validsParams,bodegaApController.createDetalleEntrada)
-    .get('/detallebodegaap',bodegaApController.getDetalleBodegaAp)
-    .put('/generarfactura/:IdEntradaBodegaAP(\\d+)',validations.crearFactura,validations.validsParams,bodegaApController.generarFactura)
-    .get('/listarfacturas',)
+//Rutas para Bodega Area Produccion
+.post('/entradabodegaap', validations.createEntradaBodegaAP, validations.validsParams, bodegaApController.createEntradaBodegaAp)
+    .post('/detalleentradabodegaap', validations.detalleEntradaBodega, validations.validsParams, bodegaApController.createDetalleEntrada)
+    .get('/detallebodegaap', bodegaApController.getDetalleBodegaAp)
+    .put('/generarfactura/:IdEntradaBodegaAP(\\d+)', validations.crearFactura, validations.validsParams, bodegaApController.generarFactura)
+    .get('/listarfacturas', )
     //Rutas para 
-    .post('/signup',validations.userSignUpValidation,validations.validsParams,AuthController.signUp)
-    .post('/signin',validations.userSignInValidation,validations.validsParams,AuthController.singIn)
-    .get('/users',AuthController.getUsers)
-    .put('/update-user/:IdUsuario(\\d+)',validations.userSignInValidation,validations.userUpdate,validations.validsParams,AuthController.updateUser)
-    .delete('/user/:IdUsuario(\\d+)',AuthController.changeStateUser)    
+    .post('/signup', validations.userSignUpValidation, validations.validsParams, AuthController.signUp)
+    .post('/signin', validations.userSignInValidation, validations.validsParams, AuthController.singIn)
+    .get('/users', AuthController.getUsers)
+    .put('/update-user/:IdUsuario(\\d+)', validations.userSignInValidation, validations.userUpdate, validations.validsParams, AuthController.updateUser)
+    .delete('/user/:IdUsuario(\\d+)', AuthController.changeStateUser)
 
 
-    module.exports=Router
-
+module.exports = Router

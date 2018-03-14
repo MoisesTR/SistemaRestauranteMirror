66943159ddc53b078e7d9582e9a6bed5eb921269
genarot <genaro_13@hotmail.es>
2018-03-14 01:05:16 -0600

    SAI-3 correccion  controlador, y agregar campo DiasCaducidad

--- ApiRest/controllers/producto_proveedor.js
+++ ApiRest/controllers/producto_proveedor.js
@@ -1,14 +1,14 @@
 const { matchedData, sanitize } = require('express-validator/filter');
-const sql = require('mssql');
-const { mssqlErrors } = require('../Utils/util');
-const db = require('../services/database');
+const { mssqlErrors }           = require('../Utils/util');
+const sql                       = require('mssql');
+const db                        = require('../services/database');
 
 function getProductoProveedorById(req, res) {
     const data = req.params;
     var aoj = [];
     db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProductoProveedor);
     db.storedProcExecute('USP_GET_PRODUCTO_PROVEEDOR', aoj)
-    .then((results) => {
+    .then(( results ) => {
         res.status(200).json({ producto: results.recordset[0] })
     }).catch((err) => {
         res.status(500).json(mssqlErrors(err));
@@ -20,7 +20,7 @@ function getProveedoresOfProducto(req, res) {
     var aoj = [];
     db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProductoProveedor);
     db.storedProcExecute('USP_GET_PRODUCTO_PROVEEDOR', aoj)
-    .then((results) => {
+    .then(( results ) => {
         res.status(200).json({ productos: results.recordset })
     }).catch((err) => {
         res.status(500).json(mssqlErrors(err));;
@@ -39,17 +39,17 @@ function getProductosProveedores(req, res) {
     });
 }
 
+var data = matchedData(req, { locations: 'body' });
 function createProductoProveedor(req, res) {
-    var data = matchedData(req, { locations: 'body' });
     var aoj = [];
     db.pushAOJParam(aoj, 'IdProducto', sql.Int, data.IdProducto)
     db.pushAOJParam(aoj, 'IdProveedor', sql.Int, data.IdProveedor)
     db.pushAOJParam(aoj, 'IdEnvase', sql.Int, data.IdEnvase)
     db.pushAOJParam(aoj, 'IdEmpaque', sql.Int, data.IdEmpaque)
-    db.pushAOJParam(aoj, 'Costo', sql.Float, data.Costo)
-    db.pushAOJParam(aoj, 'CantidadEmpaque', sql.Int, data.CantidadEmpaque)
     db.pushAOJParam(aoj, 'IdUnidadMedida', sql.Int, data.IdUnidadMedida)
-    db.pushAOJParam(aoj, 'ValorUnidadMedida', sql.Float, data.ValorUnidadMedida)
+    db.pushAOJParam(aoj, 'ValorUnidadMedida', sql.Numeric(10,5), data.ValorUnidadMedida)
+    db.pushAOJParam(aoj, 'CantidadEmpaque', sql.Int, data.CantidadEmpaque)
+    db.pushAOJParam(aoj, 'Costo', sql.Money, data.Costo)
     db.pushAOJParam(aoj, 'DiasCaducidad', sql.Int, data.DiasCaducidad)
     db.storedProcExecute('USP_CREATE_PRODUCTO_PROVEEDOR', aoj)
     .then((results) => {
@@ -74,18 +74,20 @@ function getProductosByProveedorId(req, res) {
 function updateProducto(req, res) {
     var data = req.body;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProducto)
-    db.pushAOJParam(aoj, 'IdEnvase', sql.Int, data.IdEnvase)
-    db.pushAOJParam(aoj, 'IdEmpaque', sql.Int, data.IdEmpaque)
-    db.pushAOJParam(aoj, 'Costo', sql.Float, data.Costo)
-    db.pushAOJParam(aoj, 'CantidadEmpaque', sql.Int, data.CantidadEmpaque)
-    db.pushAOJParam(aoj, 'IdUnidadMedida', sql.Int, data.IdUnidadMedida)
-    db.pushAOJParam(aoj, 'ValorUnidadMedida', sql.Float, data.ValorUnidadMedida)
+    db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProductoProveedor);
+    db.pushAOJParam(aoj, 'IdEnvase', sql.Int, data.IdEnvase);
+    db.pushAOJParam(aoj, 'IdEmpaque', sql.Int, data.IdEmpaque);
+    db.pushAOJParam(aoj, 'IdUnidadMedida', sql.Int, data.IdUnidadMedida);
+    db.pushAOJParam(aoj, 'ValorUnidadMedida', sql.Numeric(10, 5), data.ValorUnidadMedida);
+    db.pushAOJParam(aoj, 'CantidadEmpaque', sql.Int, data.CantidadEmpaque);
+    db.pushAOJParam(aoj, 'Costo', sql.Money, data.Costo);
+    db.pushAOJParam(aoj, 'DiasCaducidad', sql.Int, data.DiasCaducidad);
     db.storedProcExecute('USP_UPDATE_PRODUCTO', aoj)
         .then((results) => {
-            res.status(200).json({
-                success: 'Producto Actualizado exitosamente!!'
-            })
+            let afectadas = results.rowsAffected[0];
+            res.status(200).json(
+                ( afectadas > 0 ) ? {success: 'Producto Actualizado exitosamente!!'} : {failed: 'No se pudo actualizar la relacion producto-proveedor!' }
+            );
             console.log('Producto Actualizado con exito!');
         }).catch((err) => {
             res.status(500).json(mssqlErrors(err));
@@ -95,18 +97,17 @@ function updateProducto(req, res) {
 function changeStateProductoProveedor(req, res) {
     var data = req.body;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProductoProveedor)
+    db.pushAOJParam(aoj, 'IdProductoProveedor', sql.Int, data.IdProductoProveedor);
+    db.pushAOJParam(aoj, 'Habilitado', sql.Bit, data.Habilitado);
     db.storedProcExecute('USP_DISP_PRODUCTO_PROVEEDOR', aoj)
     .then((results) => {
-        res.status(200).json(results)
+        let afectadas = results.rowsAffected[0];
+        let accion = (Habilitado == 0) ? 'Deshabilitado' : 'Habilitado';
+        res.status(200).json( (afectadas > 0) ? {success:'Producto Proveedor '+accion+' con exito!'} :{failed:'No existe relacion entre el producto y proveedor!'} )
         console.log('Producto cambiado de estado con exito!')
     }).catch((err) => {
         res.status(500).json(mssqlErrors(err));
     });
-}
-
-function changeStateProducoPro(req, res) {
-
 }
 module.exports = {
     createProductoProveedor,

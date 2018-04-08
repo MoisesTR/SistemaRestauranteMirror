ca8c820772af6cfb3536c8360d071882b18e9733
Jake Antonio Ruiz Zeledón <jackiezeledon@gmail.com>
2018-04-07 20:10:25 -0600

    Incorporar nuevo diseño de modales

--- Sistemawebprueba/src/app/components/factura/addfactura/addfactura.component.ts
+++ Sistemawebprueba/src/app/components/factura/addfactura/addfactura.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ProductoService} from '../../../services/producto.service';
 import {Producto} from '../../../models/Producto';
+import {IMyOptions} from '../../../typescripts/pro/date-picker/interfaces';
 declare var $:any;
 
 @Component({
@@ -13,6 +14,34 @@ export class AddfacturaComponent implements OnInit {
 
   public productos : Producto[];
   public producto : Producto;
+  public myDatePickerOptions: IMyOptions = {
+    // Strings and translations
+    dayLabels: {su: 'Do', mo: 'Lu', tu: 'Mar', we: 'Mier', th: 'Jue', fr: 'Vier', sa: 'Sab'},
+    dayLabelsFull: {su: "Domingo", mo: "Lunes", tu: "Martes", we: "Miercoles", th: "Jueves", fr: "Viernes", sa: "Sabado"},
+    monthLabels: { 1: 'Ene', 2: 'Feb', 3: 'Mar', 4: 'Abr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Ago', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dic' },
+    monthLabelsFull: { 1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre" },
+
+    // Buttons
+    todayBtnTxt: "Hoy",
+    clearBtnTxt: "Limpiar",
+    closeBtnTxt: "Cerrar",
+
+    // Format
+    dateFormat: 'dd.mm.yyyy',
+
+    // First day of the week
+    firstDayOfWeek: 'mo',
+
+    // Year limits
+    minYear: 1000,
+    maxYear: 9999,
+
+    // Show Today button
+    showTodayBtn: true,
+
+    //Show Clear date button
+    showClearDateBtn: true,
+};
 
   constructor(
       private _route: ActivatedRoute

f8cfb48842cdd4790ed1789dd557b2b9ddc5aaf5
mtrigueros <moisestrigueros@hotmail.com>
2018-04-08 19:35:27 -0500

    Retornar el status 200 al actualizar un empaque

--- ApiRest/controllers/empaque.js
+++ ApiRest/controllers/empaque.js
@@ -3,57 +3,61 @@ var db = require('../services/database');
 const { mssqlErrors } = require('../Utils/util')
 const { matchedData, sanitize } = require('express-validator/filter');
 
-function getEmpaqueById(req,res){
+function getEmpaqueById(req, res) {
     const data = req.params;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdEmpaque',sql.Int,IdEmpaque);
+    db.pushAOJParam(aoj, 'IdEmpaque', sql.Int, IdEmpaque);
     db.queryExecute('SELECT IdEmpaque,NombreEmpaque,Descripcion,Habilitado FROM EMPAQUE WHERE IdEmpaque = @IdEmpaque', aoj)
-    .then((results) => {
-        res.status(200).json({empaque:results.recordset[0]}) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ empaque: results.recordset[0] })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function getEmpaques(req,res){
+
+function getEmpaques(req, res) {
     let Habilitado = req.query.Habilitado;
-    var  aoj = [];
-    db.pushAOJParam(aoj, 'Habilitado',sql.Int,Habilitado);
+    var aoj = [];
+    db.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado);
     db.storedProcExecute('USP_GET_EMPAQUES', aoj)
-    .then((results) => {
-       res.status(200).json({empaques:results.recordset}) 
-    }).catch((err) => {
-        console.log(err)
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ empaques: results.recordset })
+        }).catch((err) => {
+            console.log(err)
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function createEmpaque(req,res){
+
+function createEmpaque(req, res) {
     const data = req.body;
     var aoj = [];
-    db.pushAOJParam(aoj, 'NombreEmpaque',sql.NVarChar(50),data.NombreEmpaque);
-    db.pushAOJParam(aoj, 'Descripcion',sql.NVarChar(150),data.Descripcion);
+    db.pushAOJParam(aoj, 'NombreEmpaque', sql.NVarChar(50), data.NombreEmpaque);
+    db.pushAOJParam(aoj, 'Descripcion', sql.NVarChar(150), data.Descripcion);
     db.storedProcExecute('USP_CREATE_EMPAQUE', aoj)
-    .then((results) => {
-       res.status(200).json(results.recordset[0]) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json(results.recordset[0])
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function updateEmpaque(req,res){
+
+function updateEmpaque(req, res) {
     var data = req.body;
     var aoj = [];
-    var data    =  matchedData(req);
-    var aoj     = [];
+    var data = matchedData(req);
+    var aoj = [];
+    console.log(data);
     db.pushAOJParam(aoj, 'IdEmpaque', sql.Int, data.IdEmpaque);
     db.pushAOJParam(aoj, 'NombreEmpaque', sql.NVarChar(50), data.NombreEmpaque);
     db.pushAOJParam(aoj, 'Descripcion', sql.NVarChar(150), data.Descripcion);
     db.storedProcExecute('dbo.USP_UPDATE_EMPAQUE', aoj)
-    .then((result) => {
-        success: 'Empaque actualizado con exito!'
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    })
+        .then((result) => {
+            res.status(200).json({ success: 'Empaque actualizado con exito!!' })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        })
 }
-module.exports={
+module.exports = {
     createEmpaque,
     getEmpaqueById,
     getEmpaques,

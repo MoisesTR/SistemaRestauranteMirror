895c8257cf679eb2b6e2c500898f60894b9ace1f
mtrigueros <moisestrigueros@hotmail.com>
2018-04-29 02:50:10 -0500

    SAR-7 Corregir error en la api al capturar los datos para actualizar un cargo

--- ApiRest/controllers/cargo.js
+++ ApiRest/controllers/cargo.js
@@ -3,77 +3,82 @@ const { mssqlErrors } = require('../Utils/util');
 const db = require('../services/database');
 const sql = require('mssql');
 
-function createCargo(req,res){ 
+function createCargo(req, res) {
     var data = req.body;
-    var aoj = [];   
-    db.pushAOJParam(aoj, 'NombreCargo',sql.NVarChar(50),data.NombreCargo);
-    db.pushAOJParam(aoj, 'DescripcionCargo',sql.NVarChar(100),data.DescripcionCargo);
+    var aoj = [];
+    db.pushAOJParam(aoj, 'NombreCargo', sql.NVarChar(50), data.NombreCargo);
+    db.pushAOJParam(aoj, 'DescripcionCargo', sql.NVarChar(100), data.DescripcionCargo);
     db.storedProcExecute('USP_CREATE_CARGO', aoj)
-   .then((results) => {
-        res.status(200).json(results.recordset[0])
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    })
+        .then((results) => {
+            res.status(200).json(results.recordset[0])
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        })
 }
-function getCargos(req,res){
+
+function getCargos(req, res) {
     let Habilitado = req.query.Habilitado;
     var aoj = [];
-    db.pushAOJParam(aoj, 'Habilitado',sql.Int,Habilitado);
+    db.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado);
     db.storedProcExecute('USP_GET_CARGOS', aoj)
-    .then((results) => {
-        res.status(200).json({
-            cargos:results.recordset
-        })
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({
+                cargos: results.recordset
+            })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function updateCargo(req,res){
-    var data = matchedData(req);
-    var aoj =[];
-    db.pushAOJParam(aoj, 'IdCargo',sql.Int,data.IdCargo);
-    db.pushAOJParam(aoj, 'NombreCargo',sql.NVarChar(50),data.NombreCargo);
-    db.pushAOJParam(aoj, 'DescripcionCargo',sql.NVarChar(100),data.DescripcionCargo);
+
+function updateCargo(req, res) {
+    var data = req.body;
+    var aoj = [];
+    console.log(data);
+    db.pushAOJParam(aoj, 'IdCargo', sql.Int, data.IdCargo);
+    db.pushAOJParam(aoj, 'NombreCargo', sql.NVarChar(50), data.NombreCargo);
+    db.pushAOJParam(aoj, 'DescripcionCargo', sql.NVarChar(100), data.DescripcionCargo);
     db.storedProcExecute('USP_UPDATE_CARGO', aoj)
-    .then((results) => {
-        res.status(200).json({
-            success:'Cargo Actualizado Exitosamente!'
-        })
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({
+                success: 'Cargo Actualizado Exitosamente!'
+            })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function getCargoById(req,res){
+
+function getCargoById(req, res) {
     var data = req.params;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdCargo',sql.Int,data.IdCargo);
+    db.pushAOJParam(aoj, 'IdCargo', sql.Int, data.IdCargo);
     db.storedProcExecute('USP_GET_CARGO', aoj)
-    .then((results) => {
-        res.status(200).json({cargo:results.recordset[0]}) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ cargo: results.recordset[0] })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function changeStateCargo(req,res){
-    let IdCargo= req.params.IdCargo;
+
+function changeStateCargo(req, res) {
+    let IdCargo = req.params.IdCargo;
     let Habilitado = req.body.Habilitado;
-    console.log('IdCargo:'+IdCargo,'Habilitado:'+Habilitado);
-    var aoj=[];
-    database.pushAOJParam(aoj,'IdCargo',sql.Int,IdCargo);
-    database.pushAOJParam(aoj,'Habilitado',sql.Int,Habilitado);
-    database.storedProcExecute('USP_DISP_CARGO',aoj)
-    .then((results) => {
-        console.log(results)
-        let afectadas = results.rowsAffected[0]
-        let accion = (Habilitado == 0) ? 'Deshabilitado' : 'Habilitado';
-        res.status(200).json((afectadas > 0) ? {success:'Cargo '+accion+' con exito!'} :{failed:'No se encontro el producto solicitado!'})
-        console.log('Cargo cambiado de estado con exito!')
-    }).catch((err) => {
-       res.status(500).json( mssqlErrors(err) ); 
-       console.log('Error:',err)
-    });
+    console.log('IdCargo:' + IdCargo, 'Habilitado:' + Habilitado);
+    var aoj = [];
+    database.pushAOJParam(aoj, 'IdCargo', sql.Int, IdCargo);
+    database.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado);
+    database.storedProcExecute('USP_DISP_CARGO', aoj)
+        .then((results) => {
+            console.log(results)
+            let afectadas = results.rowsAffected[0]
+            let accion = (Habilitado == 0) ? 'Deshabilitado' : 'Habilitado';
+            res.status(200).json((afectadas > 0) ? { success: 'Cargo ' + accion + ' con exito!' } : { failed: 'No se encontro el producto solicitado!' })
+            console.log('Cargo cambiado de estado con exito!')
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+            console.log('Error:', err)
+        });
 }
-module.exports={
+module.exports = {
     createCargo,
     getCargoById,
     getCargos,

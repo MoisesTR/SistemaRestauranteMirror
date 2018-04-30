a5f1f5c4750e4473f0aceb8bf02703c59ef757fa
mtrigueros <moisestrigueros@hotmail.com>
2018-04-30 11:27:29 -0500

    SAR-32 Correccion en el controlador clasificacion API

--- ApiRest/controllers/envase.js
+++ ApiRest/controllers/envase.js
@@ -1,61 +1,65 @@
-const db    = require('../services/database');
-const sql   = require('mssql');
+const db = require('../services/database');
+const sql = require('mssql');
 const { mssqlErrors } = require('../Utils/util')
 const { matchedData, sanitize } = require('express-validator/filter');
 
-function getEnvaseById(req,res){
+function getEnvaseById(req, res) {
     var data = req.params;
     var aoj = [];
-    db.pushAOJParam(aoj, 'IdEnvase',sql.Int, data.IdEnvase);
+    db.pushAOJParam(aoj, 'IdEnvase', sql.Int, data.IdEnvase);
     db.queryExecute('SELECT IdEnvase,NombreEnvase,Descripcion,Habilitado FROM ENVASE where IdEnvase = @IdEnvase', aoj)
-    .then((results) => {
-        res.status(200).json({envase:results.recordset[0]}) 
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json({ envase: results.recordset[0] })
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function getEnvases(req,res){
+
+function getEnvases(req, res) {
     let Habilitado = req.query.Habilitado;
     console.log('Getting envases');
     var aoj = [];
-    db.pushAOJParam(aoj, 'Habilitado',sql.Int,Habilitado);
+    db.pushAOJParam(aoj, 'Habilitado', sql.Int, Habilitado);
     db.storedProcExecute('USP_GET_ENVASES', aoj)
-    .then((results) => {
-       res.status(200).json({
-           envases:results.recordset
+        .then((results) => {
+            res.status(200).json({
+                envases: results.recordset
+            });
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));;
         });
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );;
-    });
 }
-function createEnvase(req,res){
-    var data = matchedData(req, {locations:'body'});
+
+function createEnvase(req, res) {
+    var data = matchedData(req, { locations: 'body' });
     var aoj = [];
-    db.pushAOJParam(aoj, 'NombreEnvase',sql.NVarChar(50),data.NombreEnvase)
-    db.pushAOJParam(aoj, 'Descripcion',sql.NVarChar(150),data.Descripcion)
+    db.pushAOJParam(aoj, 'NombreEnvase', sql.NVarChar(50), data.NombreEnvase)
+    db.pushAOJParam(aoj, 'Descripcion', sql.NVarChar(150), data.Descripcion)
     db.storedProcExecute('USP_CREATE_ENVASE', aoj)
-    .then((results) => {
-        res.status(200).json(results.recordset[0]) 
-    }).catch((err) => {
-    
-        res.status(500).json( mssqlErrors(err) );
-    });
+        .then((results) => {
+            res.status(200).json(results.recordset[0])
+        }).catch((err) => {
+
+            res.status(500).json(mssqlErrors(err));
+        });
 }
-function updateEnvase( req, res ) {
-    var data    =  matchedData(req);
-    var aoj     = [];
+
+function updateEnvase(req, res) {
+    var data = matchedData(req, { locations: 'body' });
+    var aoj = [];
+    console.log(data);
     db.pushAOJParam(aoj, 'IdEnvase', sql.Int, data.IdEnvase);
     db.pushAOJParam(aoj, 'NombreEnvase', sql.NVarChar(50), data.NombreEnvase);
     db.pushAOJParam(aoj, 'Descripcion', sql.NVarChar(150), data.Descripcion);
-    db.storedProcExecute('dbo.UPD_UPDATE_ENVASE', aoj)
-    .then((result) => {
-        success: 'Envase actualizado con exito!'
-    }).catch((err) => {
-        res.status(500).json( mssqlErrors(err) );
-    })
+    db.storedProcExecute('dbo.USP_UPDATE_ENVASE', aoj)
+        .then((result) => {
+            success: 'Envase actualizado con exito!'
+        }).catch((err) => {
+            res.status(500).json(mssqlErrors(err));
+        })
 }
 
-module.exports={
+module.exports = {
     getEnvaseById,
     getEnvases,
     createEnvase,

851fb7faa47f172b36ab6cdb57f5861d2b94a093
mtrigueros <moisestrigueros@hotmail.com>
2018-05-05 01:54:08 -0500

    SAR-59 Incorporar servicio para eliminar una imagen

---
+++ SistemaWeb/src/app/services/delete-image-service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Global} from './global';
+import 'rxjs/add/operator/map';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class DeleteImageService {
+
+    public url: string;
+
+    constructor(private _http: HttpClient) {
+        this.url = Global.url;
+    }
+
+    deleteImage(tipo : string,img : string) : Observable<any> {
+
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'token'
+        });
+
+        return this._http.request('delete',this.url+'deleteImage/'+tipo+'/'+img,{headers:headers})
+    }
+
+
+}

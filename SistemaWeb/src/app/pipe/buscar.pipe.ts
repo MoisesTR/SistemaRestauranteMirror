7bfd48094f1e93d2a8ca00670bb5e33feaa0c01f
mtrigueros <moisestrigueros@hotmail.com>
2018-10-03 22:57:02 -0600

    Mejorando funcionalidad de la pantalla add factura

--- SistemaWeb/src/app/pipe/buscar.pipe.ts
+++ SistemaWeb/src/app/pipe/buscar.pipe.ts
@@ -1,21 +1,20 @@
-import { Injectable, Pipe, PipeTransform} from '@angular/core';
-import {isNull, isUndefined} from 'util';
-@Pipe({
+import {Injectable, Pipe, PipeTransform} from '@angular/core';
 
+@Pipe({
 	name: 'buscar'
 })
 
 @Injectable()
-export class BuscarPipe implements PipeTransform{
+export class BuscarPipe implements PipeTransform {
 
-	transform(valor:any, term:string):any{
-		if(term === undefined){
+	transform(valor: any, term: string): any {
+		if (term === undefined) {
 			return valor;
 		}
 
-		if(!isNull(valor) && !isUndefined(valor)) {
+		if (valor !== null && valor !== undefined) {
             return valor.filter( (item) => {
-                return item.NombreProducto.toLowerCase().includes(term.toLowerCase());
+                return item.CodigoProducto.toLowerCase().includes(term.toLowerCase());
             });
 		}
 

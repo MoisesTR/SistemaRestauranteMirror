88cdf1b07588aca5c209aecb21909da6313fb459
JackZeled0n <jackiezeledon@gmail.com>
2018-10-25 01:14:00 -0600

    Incorporar responsive a pantallas de agregar y actualizar productos

--- SistemaWeb/src/app/components/producto/update-producto/update-producto.component.ts
+++ SistemaWeb/src/app/components/producto/update-producto/update-producto.component.ts
@@ -105,8 +105,6 @@ showCardImg() {
         });
         f.classList.remove('col-lg-12');
         f.classList.add('col-lg-8');
-        f.classList.remove('col-md-12');
-        f.classList.add('col-md-8');
         proveedor.classList.add('select-no-margin');
         categoria.classList.add('select-no-margin');
         clasificacion.classList.add('select-no-margin');
@@ -124,8 +122,6 @@ showCardImg() {
         });
         f.classList.remove('col-lg-8');
         f.classList.add('col-lg-12');
-        f.classList.remove('col-md-8');
-        f.classList.add('col-md-12');
         proveedor.classList.remove('select-no-margin');
         categoria.classList.remove('select-no-margin');
         clasificacion.classList.remove('select-no-margin');

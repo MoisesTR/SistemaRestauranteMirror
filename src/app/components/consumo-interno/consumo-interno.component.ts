ea6736e1dc76d6ec93f686c99072dd821eb19216
JackZeled0n <jackiezeledon@gmail.com>
2019-01-07 23:41:22 -0600

    Incorporar spinner MDB

--- src/app/components/consumo-interno/consumo-interno.component.ts
+++ src/app/components/consumo-interno/consumo-interno.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsumoInternoComponent implements OnInit {
 
-  constructor() { }
+  visible: boolean = true;
+// date = new Date();
+// today = this.date.getDate() + '/' + (this.date.getMonth() + 1) + '/' + this.date.getFullYear();
 
-  ngOnInit() {
-  }
+ngOnInit() {
+setTimeout(() => {
+this.visible = false;
+}, 3000);
 
+}
 }

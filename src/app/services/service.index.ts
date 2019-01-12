8966f177baa5e580fa1193437c7e7698befa2158
Enmanuel <rogeersann97@gmail.com>
2019-01-12 02:00:36 -0600

    Servicio CambioDolar y TipoInsumo

---
+++ src/app/services/service.index.ts
@@ -0,0 +1,35 @@
+export {BodegaSucursalService} from './shared/bodega-sucursal.service';
+export {CargoService} from './shared/cargo.service';
+export {CategoriaProductoService} from './shared/categoria-producto.service';
+export {ClasificacionProductoService} from './shared/clasificacion-producto.service';
+export {ClasificacionUnidadMedidaService} from './shared/clasificacion-unidad-medida.service';
+export {DeleteImageService} from './shared/delete-image.service';
+export {EmpaqueService} from './shared/empaque.service';
+export {EnvaseService} from './shared/envase.service';
+export {EstadoEmpaqueService} from './shared/estado-empaque.service';
+export {EstadoProductoService} from './shared/estado-producto.service';
+export {FacturaService} from './shared/factura.service';
+export {MenuService} from './shared/menu.service';
+export {PermisoControlService} from './shared/permiso-control.service';
+export {ProductoService} from './shared/producto.service';
+export {ProductoProveedorService} from './shared/producto-proveedor.service';
+export {ProveedorService} from './shared/proveedor.service';
+export {RolusuarioService} from './shared/rolusuario.service';
+export {SubClasificacionProductoService} from './shared/sub-clasificacion-producto.service';
+export {SucursalService} from './shared/sucursal.service';
+export {TelefonosucursalService} from './shared/telefonosucursal.service';
+export {TrabajadorService} from './shared/trabajador.service';
+export {UnidadMedidaService} from './shared/unidad-medida.service';
+export {UploadService} from './shared/upload.service';
+export {UsuarioService} from './shared/usuario.service';
+export {AuthGuardService} from './auth/auth-guard.service';
+export {AuthService} from './auth/auth.service';
+export {ReporteService} from './shared/reporte.service';
+export {PreviousRouteService} from './shared/previous-route.service';
+export {GastoService} from './shared/gasto.service';
+export {TipoInsumoService} from './shared/tipo-insumo.service';
+export {CambioDolarService} from './shared/cambio-dolar.service'
+
+
+
+
---
+++ src/app/services/service.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import {CommonModule, DatePipe} from '@angular/common';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {
+    BodegaSucursalService
+    , CargoService
+    , CategoriaProductoService
+    , ClasificacionProductoService
+    , ClasificacionUnidadMedidaService
+    , DeleteImageService
+    , EmpaqueService
+    , EnvaseService
+    , EstadoEmpaqueService
+    , FacturaService
+    , MenuService
+    , PermisoControlService
+    , ProductoService
+    , ProductoProveedorService
+    , ProveedorService
+    , RolusuarioService
+    , SubClasificacionProductoService
+    , SucursalService
+    , TelefonosucursalService
+    , TrabajadorService
+    , UnidadMedidaService
+    , UploadService
+    , UsuarioService
+    , AuthService
+    , AuthGuardService
+    , ReporteService
+    , PreviousRouteService
+    , GastoService
+    , TipoInsumoService
+    , CambioDolarService
+
+} from './service.index';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  providers: [
+      BodegaSucursalService
+      , CargoService
+      , CategoriaProductoService
+      , ClasificacionProductoService
+      , ClasificacionUnidadMedidaService
+      , DeleteImageService
+      , EmpaqueService
+      , EnvaseService
+      , EstadoEmpaqueService
+      , FacturaService
+      , MenuService
+      , PermisoControlService
+      , ProductoService
+      , ProductoProveedorService
+      , ProveedorService
+      , RolusuarioService
+      , SubClasificacionProductoService
+      , SucursalService
+      , TelefonosucursalService
+      , TrabajadorService
+      , UnidadMedidaService
+      , UploadService
+      , UsuarioService
+      , AuthService
+      , AuthGuardService
+      , JwtHelperService
+      , DatePipe
+      , ReporteService
+      , PreviousRouteService
+      , GastoService
+      , TipoInsumoService
+      , CambioDolarService
+  ],
+  declarations: []
+})
+export class ServiceModule { }

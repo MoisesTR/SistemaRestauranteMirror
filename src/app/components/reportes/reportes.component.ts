ec9ad87abef10092a0299bd1ef65f39b7d317c81
Enmanuel <rogeersann97@gmail.com>
2019-01-31 23:05:09 -0600

    prueba reporte

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { ReporteService } from "@app/core/service.index";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
 	selector: "app-reportes",
@@ -7,18 +9,32 @@ import { ReporteService } from "@app/core/service.index";
 	styleUrls: ["./reportes.component.scss"]
 })
 export class ReportesComponent implements OnInit {
-
-    public imgProducto = 'productos.png';
-    public imgProveedor = 'proveedores.jpg';
-    public imgFactura = 'facturas.jpg';
-    public imgTrabajadores = 'trabajadores.jpg';
-    public carpetaImagenes = 'reportes';
+	public imgProducto = "productos.png";
+	public imgProveedor = "proveedores.jpg";
+	public imgFactura = "facturas.jpg";
+	public imgTrabajadores = "trabajadores.jpg";
+	public carpetaImagenes = "reportes";
 
 	constructor(private reporteService: ReporteService) {}
 
 	ngOnInit() {}
 
-	imprimirReporteProductos() {}
+	imprimirReporteProductos() {
+		var data = document.getElementById("probar");
+		html2canvas(data, { allowTaint: true, useCORS: false, scaler: 1 }).then(canvas => {
+			// Few necessary setting options
+			var imgWidth = 208;
+			var pageHeight = 295;
+			var imgHeight = (canvas.height * imgWidth) / canvas.width;
+			var heightLeft = imgHeight;
+
+			const contentDataURL = canvas.toDataURL("image/png");
+			let pdf = new jsPDF("p", "mm", "a4"); // A4 size page of PDF
+			var position = 0;
+			pdf.addImage(contentDataURL, "PNG", 0, position, imgWidth, imgHeight);
+			pdf.save("reporte.pdf"); // Generated PDF
+		});
+	}
 
 	imprimirReporteProveedores() {}
 }

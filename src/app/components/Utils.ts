7745d6af97c5e1df29f76cfb8b582ad480760158
mtrigueros <moisestrigueros@hotmail.com>
2019-02-01 23:36:43 -0600

    Validar envio de mensaje de error en la consola en el ambiente de desarrollo

--- src/app/components/Utils.ts
+++ src/app/components/Utils.ts
@@ -1,112 +1,112 @@
-import swal from 'sweetalert2';
-import * as moment from 'moment';
+import swal from "sweetalert2";
+import * as moment from "moment";
+import { environment } from "@env/environment";
 
-export class Utils{
+export class Utils {
+	static msgError(mensaje) {
+		if (!environment.production) {
+			console.log(mensaje);
+		}
 
-  static msgError(mensaje) {
-      if (mensaje === undefined) {
-        return 'El error esta indefinido';
-    } else {
-        if (Array.isArray(mensaje.error)) {
-            return mensaje.error[0].msg;
-        } else {
-            if (mensaje.error.message === undefined) {
-                return 'Error en la API';
-            } else {
-                if (mensaje.error.showMessage){
-                    return mensaje.error.message;
-                } else {
-                    return mensaje.error.message;
-                }
-            }
-        }
-    }
+		if (mensaje === undefined) {
+			return "El error esta indefinido";
+		} else {
+			if (Array.isArray(mensaje.error)) {
+				return mensaje.error[0].msg;
+			} else {
+				if (mensaje.error.message === undefined) {
+					return "Error en la API";
+				} else {
+					if (mensaje.error.showMessage) {
+						return mensaje.error.message;
+					} else {
+						return mensaje.error.message;
+					}
+				}
+			}
+		}
+	}
 
-  }
+	static msgErrorImage(mensaje, titulo = "Error") {
+		try {
+			if (mensaje === undefined) {
+				this.showMsgError("El error esta indefinido", titulo);
+			} else {
+				this.showMsgError(mensaje.error.message, titulo);
+			}
+		} catch (e) {
+			this.showMsgError(JSON.parse(mensaje).message, titulo);
+		}
+	}
 
-  static msgErrorImage(mensaje, titulo = 'Error') {
-      try {
-          if (mensaje === undefined) {
-              this.showMsgError('El error esta indefinido', titulo);
-          } else {
-              this.showMsgError(mensaje.error.message, titulo);
-          }
-      } catch (e) {
-          this.showMsgError(JSON.parse(mensaje).message, titulo);
-      }
+	// Invocacion de metodo para invocar modal y limpieza del formulario invocado
+	static invocacionModal(Modal, Formulario) {
+		Modal.show();
+		Formulario.reset();
+	}
 
-  }
+	static showMsgInfo(mensaje: string, titulo = "Información") {
+		swal(titulo, mensaje, "info").catch(swal.noop);
+	}
 
-  // Invocacion de metodo para invocar modal y limpieza del formulario invocado
-  static invocacionModal(Modal, Formulario) {
-    Modal.show();
-    Formulario.reset();
-  }
+	static showMsgError(mensaje: string, titulo = "Error") {
+		swal(titulo, mensaje, "error").catch(swal.noop);
+	}
 
-  static showMsgInfo(mensaje: string, titulo = 'Información') {
-      swal(
-          titulo,
-          mensaje,
-          'info'
-      ).catch(swal.noop);
-  }
+	static showMsgSucces(mensaje: string, titulo = "Exitoso") {
+		swal(titulo, mensaje, "success").catch(swal.noop);
+	}
 
-  static showMsgError(mensaje: string, titulo = 'Error') {
-     swal(
-         titulo,
-         mensaje,
-         'error'
-     ).catch(swal.noop);
+	static formatDateYYYYMMDD(myDate) {
+		return moment(myDate).format("YYYY-MM-DD");
+	}
 
-  }
+	static valorCampoEsValido(valor: any): any {
+		let resultado = false;
+		if (!(valor === "" || valor === null || valor === undefined)) {
+			resultado = true;
+		}
+		return resultado;
+	}
 
-  static showMsgSucces(mensaje : string, titulo = 'Exitoso') {
-      swal(
-          titulo,
-          mensaje,
-          'success'
-      ).catch(swal.noop);
-  }
+	static notNullOrUndefined(valor: any) {
+		let resultado = false;
+		if (!(valor === null || valor === undefined)) {
+			resultado = true;
+		}
+		return resultado;
+	}
 
-  static formatDateYYYYMMDD(myDate) {
-    return moment(myDate).format('YYYY-MM-DD');
-  }
+	static getYearDate(myDate) {
+		return moment(myDate, "YYYY-MM-DD").year();
+	}
 
-  static valorCampoEsValido(valor: any): any {
-      let resultado = false;
-      if (!(valor === '' || valor === null || valor === undefined)) {
-          resultado = true;
-      }
-      return resultado;
-  }
+	static replaceCharacter(cadena: string) {
+		return cadena.replace("-", "");
+	}
 
-  static notNullOrUndefined(valor: any) {
-      let resultado = false;
-      if (!(valor === null || valor === undefined)) {
-          resultado = true;
-      }
-      return resultado;
-  }
+	static printReport(idReporte, preview, catalogo, nativeWindow) {
+		nativeWindow.open(
+			"http://localhost:3000/reports?shortid=" + idReporte + "&preview=" + preview + "&catalogoApi=" + catalogo
+		);
+	}
 
-  static getYearDate(myDate) {
-      return moment(myDate, 'YYYY-MM-DD').year();
-  }
+	static printReportFactura(idReporte, preview, catalogo, idFactura, idEstadoFactura, nativeWindow) {
+		nativeWindow.open(
+			"http://localhost:3000/reports?shortid=" +
+				idReporte +
+				"&preview=" +
+				preview +
+				"&catalogoApi=" +
+				catalogo +
+				"&IdFactura=" +
+				idFactura +
+				"&IdEstadoFactura" +
+				idEstadoFactura
+		);
+	}
 
-  static replaceCharacter(cadena: string) {
-      return cadena.replace('-', '');
-  }
-
-  static printReport(idReporte, preview, catalogo, nativeWindow) {
-      nativeWindow.open('http://localhost:3000/reports?shortid=' + idReporte + '&preview=' + preview + '&catalogoApi=' + catalogo );
-  }
-
-  static printReportFactura(idReporte, preview, catalogo, idFactura, idEstadoFactura, nativeWindow) {
-      nativeWindow.open('http://localhost:3000/reports?shortid=' + idReporte + '&preview=' + preview + '&catalogoApi=' + catalogo + '&IdFactura=' + idFactura + '&IdEstadoFactura' + idEstadoFactura );
-  }
-
-  static round(value, decimals) {
-      return Number(Math.round(Number(value + 'e' + decimals)) + 'e-' + decimals);
-  }
-
-
+	static round(value, decimals) {
+		return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals);
+	}
 }

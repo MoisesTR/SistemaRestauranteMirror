e64cd0b9607122414a00cd012c0ae36c6d59bbb3
mtrigueros <moisestrigueros@hotmail.com>
2019-02-01 23:46:41 -0600

    Incorporar modelo de dato Restaurante

---
+++ src/app/models/Restaurante.ts
@@ -0,0 +1,26 @@
+export class Restaurante {
+
+    public IdRestaurante: number;
+    public IdMoneda: number;
+    public IdPais: number;
+    public IdMonedaFactura: number;
+    public IsAutoBackup: boolean;
+    public IsCuotaFija: boolean;
+    public NombRestaurante: string;
+    public DescRestaurante: string;
+    public CuotaFija: number;
+    public PorcIva: number;
+    public RazonSocial: string;
+    public SitioWeb: string;
+    public Correo: string;
+    public TelPrincipal: string;
+    public TelPrincipal2: string;
+    public FechaFundacion: Date;
+    public Login: string;
+    public Workspace: string;
+    public Habilitado: boolean;
+    public CreatedAt: Date;
+    public UpdatedAt: Date;
+
+    constructor() {}
+}

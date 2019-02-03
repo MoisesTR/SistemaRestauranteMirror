73dacdc45c5c580126426995487d70a3679a044b
mtrigueros <moisestrigueros@hotmail.com>
2019-02-02 22:05:00 -0600

    Incorporar nuevos modelos asociados a la contabilidad

---
+++ src/app/models/ContabilidadClaseCuenta.ts
@@ -0,0 +1,11 @@
+export class ContabilidadClaseCuenta {
+
+	public IdClasCuenta: number;
+	public NombClasC: string;
+	public DescClasC: string;
+	public Habilitado: boolean;
+	public CreatedAt: Date;
+	public UpdatedAt: Date;
+
+	constructor() {}
+}
---
+++ src/app/models/ContabilidadGrupoCuenta.ts
@@ -0,0 +1,10 @@
+export class ContabilidadGrupoCuenta {
+	public IdGrupo: number;
+	public IdClasCuenta: number;
+	public NombGrupo: string;
+	public Habilitado: boolean;
+	public CreatedAt: Date;
+	public UpdatedAt: Date;
+
+	constructor() {}
+}

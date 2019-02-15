d626e6189c5183156cea6de749fcc7c93061d2e3
mtrigueros <moisestrigueros@hotmail.com>
2019-02-15 01:30:05 -0600

    Incorporar error handler al interceptor

--- src/app/core/services/auth/token.interceptor.service.ts
+++ src/app/core/services/auth/token.interceptor.service.ts
@@ -5,11 +5,14 @@ import {Observable} from 'rxjs/Observable';
 import {tap} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {NGXLogger} from 'ngx-logger';
+import {throwError} from 'rxjs';
+import {Utils} from '@app/components/Utils';
+import {SpinnerService} from '@app/core/service.index';
 
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
 
-    constructor(public auth: AuthService,  public router: Router, public logger: NGXLogger) {}
+    constructor(public auth: AuthService, public spinnerService: SpinnerService, public router: Router, public logger: NGXLogger) {}
     intercept(request: HttpRequest<any>, next: HttpHandler):
         Observable<HttpEvent<any>> {
 
@@ -24,15 +27,24 @@ export class TokenInterceptorService implements HttpInterceptor {
             tap(
                 (response: HttpEvent<any>) => {},
                 (err: any) => {
-
+                    let errorMessage = '';
                     if (err instanceof HttpErrorResponse) {
-
-                        if (err.status === 401) {
-                            this.auth.logout();
+                        if (err.error instanceof ErrorEvent) {
+                            // client-side error
+                            errorMessage = `Error: ${err.error.message}`;
+                        } else {
+                            // server-side error
+                            errorMessage = `Error Code: ${err.status}\nMessage: ${err.message}`;
+                            if (err.status === 401) {
+                                this.auth.logout();
+                            }
                         }
+                        if (this.spinnerService.status) this.spinnerService.display(false);
+                        Utils.showMsgError(errorMessage, 'Ops... Ha ocurrido un error!');
+                        return throwError(errorMessage);
                     }
                 }
-            )
-        );
+            ),
+        )
     }
 }
